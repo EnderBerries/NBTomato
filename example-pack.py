68cdd04f428ac71402fd1f3db1237cb43f4731fb
jiajinxuan2009 <jiajinxuan209430@outlook.com>
2024-05-05 19:44:01 +0800

    modified:   example-pack.py

--- example-pack.py
+++ example-pack.py
@@ -8,12 +8,26 @@ The following is a detailed explanation of the parameters:\n
 Here is English Comparison Table:
 
     Lang="en_us"
-    translation={
-        "en_us":{
-            "trans.pack.name":"English(US)",
-            "trans.finished_change_language":"Successfully set the language",
-            "trans.main.get_mcmeta_path":"Please enter the \"pack.mcmeta\" file path:",
-        }
+    translation = {  
+        "en_us": {  
+            "trans.pack.name": "English (US)",  
+            "trans.exit": "Exited",  
+            "trans.finished_change_language": "Language successfully set",  
+            "trans.main.get_mcmeta_path": "Please enter the path to the pack.mcmeta file:",  
+            "trans.ui.input": ">>>",  
+            "trans.ui.on": "On: 1",  
+            "trans.ui.off": "Off: 2",  
+            "trans.ui.menu": "Enter the corresponding code to perform the desired operation",  
+            "trans.ui.not_allow": "Please enter an allowed value",  
+            "trans.ui.menu.settings": "Settings: 1",  
+            "trans.ui.menu.update": "Update data pack: 2",  
+            "trans.ui.menu.exit": "Exit: 3",  
+            "trans.ui.menu.settings.languages": "Language: 1",  
+            "trans.ui.menu.settings.show_detailed_info": "Show detailed information: 2",  
+            "trans.ui.menu.settings.back": "Back: 3",  
+            "trans.ui.saved": "Saved!",  
+            "trans.ui.not_saved": "No changes made!",  
+        }  
     }
 
 After you complete the translation,You should use "python -m" to compile your program into ".pyc" file\n
@@ -25,9 +39,23 @@ Finally,run "NBTomato.py",then you should see your language in the menu
 #Fill the things below
 Lang="<Lang>"
 translation={
-    "en_us":{
+    "<Lang>":{
         "trans.pack.name":"<pack>",
-        "trans.finished_change_language":"<translation>",
-        "trans.main.get_mcmeta_path":"<translation>",
+        "trans.exit": "<translation>",  
+        "trans.finished_change_language": "<translation>",  
+        "trans.main.get_mcmeta_path": "<translation>",  
+        "trans.ui.input": ">>>",  
+        "trans.ui.on": "<translation>",  
+        "trans.ui.off": "<translation>",  
+        "trans.ui.menu": "<translation>",  
+        "trans.ui.not_allow": "<translation>",  
+        "trans.ui.menu.settings": "<translation>",  
+        "trans.ui.menu.update": "<translation>",  
+        "trans.ui.menu.exit": "<translation>",  
+        "trans.ui.menu.settings.languages": "<translation>",  
+        "trans.ui.menu.settings.show_detailed_info": "<translation>",  
+        "trans.ui.menu.settings.back": "<translation>",  
+        "trans.ui.saved": "<translation>!",  
+        "trans.ui.not_saved": "<translation>", 
     }
 }
